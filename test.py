efccae133f543eaac38d5e82b331a9a30626c8ff
Hello World <362378526@qq.com>
2018-11-01 04:51:57 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+"""
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
+
+turtle.mainloop()
+
+"""
+
+while True:
+    a = int(input("please enter number a: "))
+    b = int(input("please enter number b: "))
+    print("a + b = " + str(a + b))
